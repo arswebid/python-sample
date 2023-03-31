1a904a5b414d88be9d1ad79805d0d9901cfa446e
Ars Web <128975695+arswebid@users.noreply.github.com>
2023-04-01 04:34:48 +0700

    Create looping-model.py

---
+++ looping-model.py
@@ -0,0 +1,8 @@
+# Loop through a range of numbers
+for i in range(10):
+    print(i)
+
+# Loop through a list of names
+names = ["Alice", "Bob", "Charlie"]
+for name in names:
+    print("Hello, " + name + "!")
