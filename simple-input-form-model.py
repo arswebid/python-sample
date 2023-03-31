0c082edecb99ca5b0a51994b115408cb251f4c45
Ars Web <128975695+arswebid@users.noreply.github.com>
2023-04-01 04:41:01 +0700

    Create simple-input-form-model.py
    In this example, we're using the input() function to prompt the user to enter their name, age, and email address. The user can type in their responses and press Enter to submit each input.
    
    We're then storing the inputs in variables named name, age, and email. Finally, we're printing the inputs using the print() function.
    
    You can customize the input prompt and message to suit your needs. You can also use Python's string formatting or concatenation features to format the input message or output in a specific way.

---
+++ simple-input-form-model.py
@@ -0,0 +1,9 @@
+# Get input from the user
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+email = input("Enter your email address: ")
+
+# Print the inputs
+print("Name:", name)
+print("Age:", age)
+print("Email:", email)
